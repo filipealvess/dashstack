7992d23fb6a9da88e763248cb7d79c64e191151b
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-05-15 16:52:31 -0300

    Use nunito-sans google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,17 @@ import "@/styles/global.css";
 import StyledJsxRegistry from "@/registry";
 
 import type { Metadata } from "next";
+import { Nunito_Sans } from "next/font/google";
 
 export const metadata: Metadata = {
     title: "DashStack",
     description: "Painel de controle",
 };
+
+const font = Nunito_Sans({
+    subsets: ["latin"],
+    weight: ["400", "600", "700"],
+});
 
 export default function RootLayout({
     children,
@@ -15,7 +21,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="pt-BR">
-            <body>
+            <body className={font.className}>
                 <StyledJsxRegistry>{children}</StyledJsxRegistry>
             </body>
         </html>
