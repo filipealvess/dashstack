58e85a8ef41c2fdf3782b128531fb6a2c7c00f9d
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-05-16 19:15:03 -0300

    Redirect from home to sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import { redirect } from "next/navigation";
+import { Routes } from "@/constants/routes";
+
 export default function Home() {
-    return <h1>DashStack</h1>;
+    redirect(Routes.signin);
 }
