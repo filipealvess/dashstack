2b3becb6654d4edfcc6ca5f5c4658cdd4f7e9cdf
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-05-16 19:11:11 -0300

    Create spacer component

---
+++ src/components/styled/Spacer/index.ts
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+import type { IProps } from "@/components/styled/Spacer/index.d";
+
+export const Spacer = styled.div<IProps>`
+    ${({ height }) =>
+        height !== undefined &&
+        css`
+            height: ${height}px;
+        `}
+`;
