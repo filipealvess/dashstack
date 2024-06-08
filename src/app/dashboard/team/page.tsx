1eb7f21df193d23f5f04355376f9b4a01b1ea0c5
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-06-08 19:00:44 -0300

    Create initial structure of team page

---
+++ src/app/dashboard/team/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as Styles from "@/app/dashboard/team/styles";
+
+function Index() {
+    return (
+        <Styles.Container>
+            <h1>Team</h1>
+        </Styles.Container>
+    );
+}
+
+export default Index;
