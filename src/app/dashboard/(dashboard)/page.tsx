fe92e3095efb4b5bccab2b652250e7bfea76231e
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-06-20 18:51:24 -0300

    Use summary cards in dashboard home page

--- src/app/dashboard/(dashboard)/page.tsx
+++ src/app/dashboard/(dashboard)/page.tsx
@@ -1,11 +1,53 @@
 "use client";
 
+import { BarChart2, Clock, Package, Users } from "react-feather";
 import * as Styles from "@/app/dashboard/(dashboard)/styles";
+import SummaryCard from "@/components/cards/Summary";
 
 function Index() {
     return (
         <Styles.Container>
-            <h1>Dashboard</h1>
+            <Styles.Header>
+                <h1>Dashboard</h1>
+            </Styles.Header>
+
+            <Styles.Summary>
+                <SummaryCard
+                    color="#3D42DF"
+                    icon={<Users />}
+                    rate={8.5}
+                    rateLabel="Up from yesterday"
+                    title="Total User"
+                    value="40,689"
+                />
+
+                <SummaryCard
+                    color="#DDA727"
+                    icon={<Package />}
+                    rate={1.3}
+                    rateLabel="Up from past week"
+                    title="Total Order"
+                    value="10293"
+                />
+
+                <SummaryCard
+                    color="#4AD991"
+                    icon={<BarChart2 />}
+                    rate={-4.3}
+                    rateLabel="Up from yesterday"
+                    title="Total Sales"
+                    value="$89,000"
+                />
+
+                <SummaryCard
+                    color="#FF9066"
+                    icon={<Clock />}
+                    rate={1.8}
+                    rateLabel="Up from yesterday"
+                    title="Total Pending"
+                    value="2040"
+                />
+            </Styles.Summary>
         </Styles.Container>
     );
 }
--- src/app/dashboard/(dashboard)/styles.ts
+++ src/app/dashboard/(dashboard)/styles.ts
@@ -1,6 +1,49 @@
 import styled from "styled-components";
+import {
+    Container as SummaryCard,
+    Footer as SummaryCardFooter,
+} from "@/components/cards/Summary/styles";
 
 export const Container = styled.main`
     padding: 24px;
     overflow-y: auto;
+`;
+
+export const Header = styled.header`
+    margin-bottom: 24px;
+
+    h1 {
+        font-size: 2.4rem;
+        font-weight: 700;
+    }
+`;
+
+export const Summary = styled.section`
+    display: flex;
+    gap: 16px;
+
+    ${SummaryCard} {
+        flex: 1;
+
+        @media (max-width: 800px) {
+            flex: none;
+            width: calc(50% - 8px);
+        }
+
+        @media (max-width: 450px) {
+            width: 100%;
+        }
+    }
+
+    ${SummaryCardFooter} {
+        @media (max-width: 900px) and (min-width: 800px) {
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 0;
+        }
+    }
+
+    @media (max-width: 800px) {
+        flex-wrap: wrap;
+    }
 `;
