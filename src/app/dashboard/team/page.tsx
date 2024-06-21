7f7925f010f9f1cfcbba830d7e913a7bc38d6c8d
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-06-21 19:37:59 -0300

    Redirect to new team member page

--- src/app/dashboard/team/page.tsx
+++ src/app/dashboard/team/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import users from "@/app/dashboard/team/mock";
 import * as Styles from "@/app/dashboard/team/styles";
 import UserCard from "@/components/cards/User";
 import Button from "@/components/form/Button";
+import { Routes } from "@/constants/routes";
 
 function Index() {
+    const router = useRouter();
+
     return (
         <Styles.Container>
             <Styles.Header>
                 <h1>Team</h1>
 
-                <Button text="Add Member" />
+                <Button
+                    onClick={() => router.push(Routes.addMember)}
+                    text="Add Member"
+                />
             </Styles.Header>
 
             <Styles.Grid>
