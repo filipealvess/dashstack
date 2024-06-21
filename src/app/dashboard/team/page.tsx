5ef90d123a377bb588238b55a01e235e230c1877
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-06-21 17:46:10 -0300

    Display members grid in dashboard team page

--- src/app/dashboard/team/page.tsx
+++ src/app/dashboard/team/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import users from "@/app/dashboard/team/mock";
 import * as Styles from "@/app/dashboard/team/styles";
+import UserCard from "@/components/cards/User";
+import Button from "@/components/form/Button";
 
 function Index() {
     return (
         <Styles.Container>
-            <h1>Team</h1>
+            <Styles.Header>
+                <h1>Team</h1>
+
+                <Button text="Add Member" />
+            </Styles.Header>
+
+            <Styles.Grid>
+                {users.map((user) => (
+                    <UserCard key={user.email} {...user} />
+                ))}
+            </Styles.Grid>
         </Styles.Container>
     );
 }
--- src/app/dashboard/team/styles.ts
+++ src/app/dashboard/team/styles.ts
@@ -3,4 +3,36 @@ import styled from "styled-components";
 export const Container = styled.main`
     padding: 24px;
     overflow-y: auto;
+    overflow-x: hidden;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h1 {
+        font-size: 2.4rem;
+        font-weight: 700;
+    }
+`;
+
+export const Grid = styled.section`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 16px;
+    max-width: 100%;
+
+    @media (max-width: 900px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    @media (max-width: 700px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media (max-width: 480px) {
+        grid-template-columns: 1fr;
+    }
 `;
