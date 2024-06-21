52e570a07ded450bb9099dae34c7976770301317
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-06-21 17:45:16 -0300

    Create mock with team members

---
+++ src/app/dashboard/team/mock.ts
@@ -0,0 +1,66 @@
+import type { IProps as IMember } from "@/components/cards/User/index.d";
+
+const members: IMember[] = [
+    {
+        avatar: require("@/assets/images/user-1.jpeg"),
+        email: "janick_parisian@yahoo.com",
+        name: "Jason Price",
+        role: "Admin",
+    },
+    {
+        avatar: require("@/assets/images/user-2.jpeg"),
+        email: "sibyl_kozey@gmail.com",
+        name: "Jukkoe Sisao",
+        role: "CEO",
+    },
+    {
+        avatar: require("@/assets/images/user-3.jpeg"),
+        email: "feil.wallace@kunde.us",
+        name: "Harriet King",
+        role: "CTO",
+    },
+    {
+        avatar: require("@/assets/images/user-4.jpeg"),
+        email: "nadia_block@hotmail.com",
+        name: "Lenora Benson",
+        role: "Lead",
+    },
+    {
+        avatar: require("@/assets/images/user-5.jpeg"),
+        email: "kemmer.hattie@cremin.us",
+        name: "Olivia Reese",
+        role: "Strategist",
+    },
+    {
+        avatar: require("@/assets/images/user-6.jpeg"),
+        email: "loraine.koelpin@tromp.io",
+        name: "Bertha Valdez",
+        role: "CEO",
+    },
+    {
+        avatar: require("@/assets/images/user-7.jpeg"),
+        email: "nannie_west@estrella.tv",
+        name: "Harriett Payne",
+        role: "Digital Marketer",
+    },
+    {
+        avatar: require("@/assets/images/user-8.jpeg"),
+        email: "delmer.kling@gmail.com",
+        name: "George Bryant",
+        role: "Social Media",
+    },
+    {
+        avatar: require("@/assets/images/user-9.jpeg"),
+        email: "octavia.nienow@gleichner.net",
+        name: "Patrick Padilla",
+        role: "Social Media",
+    },
+    {
+        avatar: require("@/assets/images/user-10.jpeg"),
+        email: "wiegand.leonor@herman.us",
+        name: "Howard Adkins",
+        role: "CEO",
+    },
+];
+
+export default members;
